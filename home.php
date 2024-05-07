7b06e588000178eebfd23c907ab85981e494e0ff
capellipaolo <121513425+POL-laway@users.noreply.github.com>
2024-05-07 15:47:55 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<link href="stili/stilehome.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="contenitore">
+
+<header>
+<div id="logo">
+<img src="immagini/logo.png" alt="logo">
+
+</div>
+
+</header>
+
+<div id="centro">
+    <div id="menu">
+    <nav>
+        <li class="selezionato"><a href="home.php">HOME</a></li>
+        <li><a href="">AUTORI</a></li> 
+        <li><a href="">OPERE</a></li> 
+        <li><a href="">TEMI</a></li>
+        <div class="dropdown">
+  <button class="dropbtn" >SALE</button>
+  <div class="dropdown-content">
+    <a href="#">Inserisci</a>
+    <a href="#">Modifica</a>
+    <a href="#">Elimina</a>
+</div>
+</div>
+       
+      </nav>
+    </div>
+    <div id="box">
+    <div id="home">
+        <p>Benvenuti al Museo CdoubleS: </br>un luogo dove la storia prende vita e l'arte parla al cuore. Esplora le nostre collezioni uniche che abbracciano secoli di cultura e creatività. Da antiche reliquie a capolavori contemporanei, ogni opera racconta una storia straordinaria.
+        </p>
+</br>
+</br>
+</br>
+<p> In un viaggio attraverso le epoche, dalla magnificenza dell'antichità alla vivacità del presente. Attraverso mostre interattive, eventi culturali e laboratori creativi, il Museo CdoubleS ti invita a esplorare il mondo con occhi nuovi.</p>
+</br>
+</br>
+</br><p>
+Che tu sia un appassionato d'arte, uno studioso della storia o semplicemente un curioso viaggiatore, troverai ispirazione e conoscenza nelle profondità del nostro museo. Unisciti a noi e scopri il fascino senza tempo dell'umanità attraverso le meraviglie esposte al Museo CdoubleS.</p>
+    </div>
+        <div id="filtro" style="display:none;">
+        <label for="cerca">CERCA: </label><input type="search" name="cerca" value="">
+
+        </div>
+
+        <div id="contenuto" style="	display:none;">
+                CONTENUTO
+        </div>
+    </div>
+    
+
+</div>
+
+</div>
+
+
+<footer>
+<div class="footer-container">
+    <div class="footer-section">
+      <h3>Contatti</h3>
+      <p>Indirizzo: Via dell'Arte, 123 - Città</p>
+      <p>Telefono: +39 0123 456789</p>
+      <p>Email: info@museocdoubles.it</p>
+    </div>
+    <div class="footer-section">
+      <h3>Orari di Apertura</h3>
+      <p>Lunedì - Venerdì: 9:00 - 18:00</p>
+      <p>Sabato - Domenica: 10:00 - 20:00</p>
+      <p>Chiuso il Martedì</p>
+    </div>
+    <div class="footer-section">
+      <h3>Seguici</h3>
+      <p>Seguici sui social media per rimanere aggiornato sulle nostre mostre e eventi.</p>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2024 Museo CdoubleS. Tutti i diritti riservati.</p>
+  </div>
+</footer>
+</body>
